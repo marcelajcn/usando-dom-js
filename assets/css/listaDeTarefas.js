6b8e8640ca54eb544b35a51933efd9832f122dd6
marcelajcn <hdsmar27@gmail.com>
2023-07-13 14:57:03 -0300

    first commit

---
+++ assets/css/listaDeTarefas.js
@@ -0,0 +1,8 @@
+const novaTarefa = document.querySelector('[data-form-button]');
+
+const input = document.querySelector('[data-form-input]');
+
+console.log(input);
+
+novaTarefa.addEventListener('click', ()=> {console.log('fui clicado')});
+
